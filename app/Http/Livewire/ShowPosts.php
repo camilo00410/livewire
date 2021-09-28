f87ac517a8011474091a81957c074a26b3fdab97
Camilo loaiza <camiloandres00410@gmail.com>
2021-09-27 23:10:24 -0500

    first commit

---
+++ app/Http/Livewire/ShowPosts.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Post;
+
+class ShowPosts extends Component
+{
+
+    // public $name;
+
+    // public function mount($name){
+    //     $this->name = $name;
+    // }
+
+    public $search;
+    public $sort = 'id';
+    public $direction ='desc';
+
+    public function render()
+    {
+        $posts = Post::where('title', 'like', '%' . $this->search . '%')
+                    ->orWhere('content', 'like', '%' . $this->search . '%')
+                    ->orderBy($this->sort, $this->direction)
+                    ->get();
+        return view('livewire.show-posts', compact('posts'));
+                // ->layout('layouts.base');
+    }
+
+    public function order($sort){
+        if($this->sort = $sort){
+            if($this->direction == 'desc'){
+                $this->direction = 'asc';
+            }else{
+                $this->direction = 'desc';
+            }
+        }else{
+            $this->sort = $sort;
+            $this->direction = 'asc';
+        }        
+    }
+}
---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,110 @@
+<div>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    {{$search}}
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <!-- This example requires Tailwind CSS v2.0+ -->
+            <x-table>
+
+                <div class="px-6 py-4">
+                    <x-jet-input type="text" wire:model="search" class="w-full" placeholder="Escriba que quiere buscar"/>
+                </div>
+
+                @if($posts->count())
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                            <th scope="col" class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="order('id')">
+                                ID
+                                {{-- sort --}}
+                                @if($sort == 'id')                        
+                                    @if($direction == 'asc')
+                                        <i class=" float-right fas fa-sort-alpha-up-alt mt-1"></i>
+                                    @else
+                                        <i class=" float-right fas fa-sort-alpha-down-alt mt-1"></i>
+                                    @endif
+
+                                @else
+                                    <i class=" float-right fas fa-sort mt-1"></i>
+                                @endif
+                               
+
+                            </th>
+                            <th scope="col" class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="order('title')">
+                                Title
+
+                            {{-- sort --}}
+                           @if($sort == 'title')                        
+                                @if($direction == 'asc')
+                                    <i class=" float-right fas fa-sort-alpha-up-alt mt-1"></i>
+                                @else
+                                    <i class=" float-right fas fa-sort-alpha-down-alt mt-1"></i>
+                                @endif
+
+                            @else
+                                <i class=" float-right fas fa-sort mt-1"></i>
+                            @endif
+                               
+                            </th>
+                            <th scope="col" class="cursor-pointer px-6 py-b3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="order('content')">
+                                Content
+                                {{-- sort --}}
+                                    @if($sort == 'content')                        
+                                        @if($direction == 'asc')
+                                            <i class=" float-right fas fa-sort-alpha-up-alt mt-1"></i>
+                                        @else
+                                            <i class=" float-right fas fa-sort-alpha-down-alt mt-1"></i>
+                                        @endif
+    
+                                    @else
+                                        <i class=" float-right fas fa-sort mt-1"></i>
+                                    @endif
+                            </th>
+
+                            <th scope="col" class="relative px-6 py-3">
+                                <span class="sr-only">Edit</span>
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            @foreach($posts as $post)
+                                <tr>                    
+                                    <td class="px-6 py-4 ">
+                                        <div class="text-sm text-gray-900">{{$post->id}}</div>
+                                    </td>
+                                    <td class="px-6 py-4 ">
+                                        <div class="text-sm text-gray-900">{{$post->title}}</div>                                    
+                                    </td>
+                                    <td class="px-6 py-4  text-sm text-gray-500">
+                                        <div class="text-sm text-gray-900">{{$post->content}}</div>                                    
+                                    </td>
+                                    <td class="px-6 py-4  text-right text-sm font-medium">
+                                        <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                            <!-- More people... -->
+                        </tbody>
+                    </table>
+                @else
+                    <div class="px-6 py-4">
+                        No existe ningun registro coincidente
+                    </div>
+                @endif
+
+            </x-table>
+            
+
+    </div>
+
+</div>
