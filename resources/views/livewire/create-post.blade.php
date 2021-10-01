44f3596b83268f66808cf36a1009bc62bd17984a
Camilo loaiza <camiloandres00410@gmail.com>
2021-10-01 00:14:32 -0500

    Subir imagenes

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -12,6 +12,17 @@
         </x-slot>
 
         <x-slot name="content">
+
+        <div wire:loading wire:target="image" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+            <strong class="font-bold">Imagen Cargando!</strong>
+            <span class="block sm:inline">Espere un momento hasta que la imagen se haya procesado.</span>
+            
+        </div>
+
+            @if($image)
+                <img class="mb-4" src="{{$image->temporaryUrl()}}" alt="">
+            @endif
+
             <div class="mb-4">
                 <x-jet-label value="Titulo del post"/>
                 <x-jet-input type="text" class="w-full" wire:model="title"/>
@@ -26,6 +37,12 @@
                 <x-jet-input-error for="content"/>
 
             </div>
+
+            <div>
+                <input type="file" wire:model="image" id="{{$identificador}}">
+                <x-jet-input-error for="image"/>
+            </div>
+
         </x-slot>
 
         <x-slot name="footer">
@@ -38,7 +55,7 @@
                 Crear post
             </x-jet-danger-button> -->
 
-            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save" class="disabled:opacity-25">
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save, image" class="disabled:opacity-25">
                 Crear post
             </x-jet-danger-button>
            
